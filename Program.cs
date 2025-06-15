4d3474605c44af2a91ea8e341da3c4a89e96a0c9
Hasan <hasan.ackl92@gmail.com>
2025-06-15 19:00:23 +0300

    Initial commit - full TodoAPI project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using TodoAPI.AppDataContext;
+using TodoAPI.Interface;
+using TodoAPI.Middleware;
+using TodoAPI.Models;
+using TodoAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
+builder.Services.AddSingleton<TodoDbContext>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+builder.Services.AddLogging();
+
+builder.Services.AddScoped<ITodoServices, TodoServices>();
+
+var app = builder.Build();
+
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider;
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
